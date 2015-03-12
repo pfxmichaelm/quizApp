67cacf4a939811a186b0ec10807192248a15afe5
Michael Miller <pfxmichaelm@gmail.com>
2015-03-12 16:02:31 -0600

    checkbox styling issues

--- js/ap.js
+++ js/ap.js
@@ -5,35 +5,35 @@ $(document).ready(function() {
 		answers: ["Round globs of cured rubber", "Thin leather filled with ground corn cobs","Thin leather covering stuffed with feathers","Nine layers of leather wrapped around a small stone"],
 		qnum: 0,
 		correct: 3,
-		fact: "The first golf balls were made of thin leather stuffed with feathers. Tightly-packed feathers made balls that flew the farthest. Feather balls were used until 1848."
+		fact: "Question #1 fact - The first golf balls were made of thin leather stuffed with feathers. Tightly-packed feathers made balls that flew the farthest. Feather balls were used until 1848."
 		},
 		{
 		question: "#2 - How many dimples are on a regulation golf ball?", 
 		answers: ["336", "375", "409", "418"],
 		qnum: 1,
 		correct: 1,
-		fact: "There are 336 dimples on a regulation golf ball"
+		fact: "Question #2 fact - There are 336 dimples on a regulation golf ball"
 		},
 		{
 		question: "#3 - How old was Tiger Woods when he recorded his first ace?",
 		answers: ["Six years old", "Seven years old", "Eight years old", "Ten years old"],
 		qnum: 2,
 		correct: 3,
-		fact: "Tiger Woods snagged his first ace at the tender age of eight years old."
+		fact: "Question #3 fact - Tiger Woods snagged his first ace at the tender age of eight years old."
 		},
 		{
 		question: "#4 - What impact does temperature have on the travel of a golf ball?", 
 		answers: ["No effect", "Small decrease in distance", "Small increase in distance", "Significant increase in distance"],
 		qnum: 3,
 		correct: 4,
-		fact: "A golfer swinging a club at around 100 mph will carry the driver up to eight yards longer for each increase in air temperature of 25°F.",
+		fact: "Question #4 fact - A golfer swinging a club at around 100 mph will carry the driver up to eight yards longer for each increase in air temperature of 25°F.",
 		},		
 		{
 		question: "#5 - Where does the term birdie come from?", 
 		answers: ["America", "England", "Scotland", "Ireland"],
 		qnum: 4,
 		correct: 1,
-		fact: "The term birdie comes from an American named Ab Smith. While playing 1899, he played what he described as a 'bird of a shot', which became 'birdie' over time."
+		fact: "Question #5 fact - The term birdie comes from an American named Ab Smith. While playing 1899, he played what he described as a 'bird of a shot', which became 'birdie' over time."
 	}]
 
 	var idx = 0,
